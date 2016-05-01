a4173e8f7cf0c1f2b943f008ad7924c3bd119947
Nancy McNamara <nmcnama1@nd.edu>
2016-05-01 03:37:29 -0400

    getting closer! cannon works on p2

--- p2.py
+++ p2.py
@@ -35,6 +35,7 @@ class GameSpace:
 		self.clock = pygame.time.Clock()
 		self.rain = Rain(self)
 		self.player1 = Player1(self)
+		self.player2 = Player2(self)
 
 		self.bg = pygame.image.load("media/"+mode['background_image'])
 		self.bg = pygame.transform.scale(self.bg, mode['background_scale'])
@@ -48,10 +49,12 @@ class GameSpace:
 		mx, my = pygame.mouse.get_pos()
 
 
-		for guy in self.rain.drops:
-			if collision(guy.rect.center, [self.player1.rect.center[0]+mode['catcher_offset'][0], self.player1.rect.center[1]+mode['catcher_offset'][1]]):
-				self.rain.drops.remove(guy)
-				self.score2+=1
+		for bullet in self.player2.lasers:
+			for guy in self.rain.drops:
+				if collision(guy.rect.center, bullet.rect.center):
+					self.rain.drops.remove(guy)
+					self.player2.lasers.remove(bullet)
+					self.score2+=1
 
 		#4. clock tick regulation (framerate)
 		self.clock.tick(60)
@@ -60,14 +63,24 @@ class GameSpace:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
-
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				self.player2.tofire = True
+			if event.type == pygame.MOUSEBUTTONUP:
+				self.player2.tofire = False
 
 		#6. send a tick to every game object
 		self.rain.tick()
 		self.player1.tick()
+		self.player2.tick()
+		for laser in self.player2.lasers:
+			laser.tick()
+#				self.player2.lasers.remove(laser)
 		#7. finally, display game object
 		self.screen.blit(self.bg, (0,0))
 		self.screen.blit(self.player1.image, self.player1.rect)
+		for laser in self.player2.lasers:
+			self.screen.blit(laser.image, laser.rect)
+		self.screen.blit(self.player2.image, self.player2.rect)
 		lt = pygame.font.Font('freesansbold.ttf',115)
 		textSurf = lt.render(str(self.score2), True, (100, 100, 100))
 		TextRect = textSurf.get_rect()
@@ -131,12 +144,74 @@ class Box(pygame.sprite.Sprite):
 		self.y = center[1]+mode['box_offset'][1]
 		self.rect.center = [self.x,self.y]
 
+class Player2(pygame.sprite.Sprite):
+
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.realx = 1
+		self.realy = 1
+		self.gs = gs
+		self.image = pygame.image.load("media/canon2.jpg")
+		self.rect = self.image.get_rect()
+		self.rect.center = (600, 205)
+		self.lasers = []
+		#keep original image to limit resize errors
+		self.orig_image = self.image
+
+		#if I can fire laser beams, this flag will say whether I should be firing them right now
+		self.tofire = False
+
+	def tick(self):
+		#get the mouse x and y position on the screen
+		mx, my = pygame.mouse.get_pos()
+
+		for guy in self.lasers:
+			if guy.rect.center[0] < -20 or guy.rect.center[0] > 660:
+				self.lasers.remove(guy)	
+				print "MOTHERFUCKER"
+			elif guy.rect.center[1] < -20 or guy.rect.center[1] > 500:
+				self.lasers.remove(guy)
+		#this conditional prevents movement while firing
+		if self.tofire == True:
+			self.realx=mx
+			self.realy=my
+	
+			#code to emit a laser beam block
+			xSlope = self.realx-self.rect.center[0]
+			ySlope = self.realy-self.rect.center[1]
+			total = math.fabs(xSlope)+math.fabs(ySlope)
+			self.lasers.append(Laser(self,self.rect.center[0],self.rect.center[1],xSlope/total, ySlope/total))
+			self.tofire = False
+		else:	
+			#code to calculate the angle between my current direction and the mouse position (see math.atan2)
+			angle = math.atan2(my-self.rect.center[1],mx-self.rect.center[0])*-180/math.pi+211.5
+			#self.image = rot_center(self.orig_image, angle)	
+			self.image = pygame.transform.rotate(self.orig_image, angle)
+			self.rect = self.image.get_rect(center = self.rect.center)
+			self.tofire = False
+
+class Laser(pygame.sprite.Sprite):
+	def __init__(self, gs=None, xc=320, yc=240, xm=1, ym=1):
+		pygame.sprite.Sprite.__init__(self)
+		xc=xc+xm*32
+		yc=yc+ym*32
+		self.xm=xm*10
+		self.ym=ym*10
+		self.gs = gs
+		self.image = pygame.image.load("media/cannonball.png")
+		self.rect = self.image.get_rect()
+		self.rect.center=[xc,yc]
+
+	def tick(self):
+		self.rect = self.rect.move([self.xm,self.ym])
+	
+		
 def dist(x1, y1, x2, y2):
 	return ((y2-y1)**2+(x2-x1)**2)**.5
 
-def collision(ball_center, catcher_point):
-	distance = dist(ball_center[0], ball_center[1], catcher_point[0], catcher_point[1]) 
-	if distance<=25:
+def collision(ball_center, bullet_center):
+	distance = dist(ball_center[0], ball_center[1], bullet_center[0], bullet_center[1]) 
+	if distance<=50:
 		return True
 	else:
 		return False
@@ -145,16 +220,10 @@ class ClientConnection(Protocol):
 	def __init__(self, client):
 		self.client = client
 	def dataReceived(self, data):
-#		print 'game received from client'
-#		big = zlib.decompress(data)
-#		self.gs = pickle.loads(big)
-#		print 'game initialized'
-#		self.gs.main()
 		data =  pickle.loads(data)
 		self.client.player1.rect.center = data[0]
 		self.client.player1.box.rect.center = data[1]
 		self.client.rain.addNew = data[2]
-	#	self.client.player1.box.rect.center = data[1]
 	def connectionMade(self):
 		self.transport.write('player 2 connected')
 	def connectionLost(self, reason):
