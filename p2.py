1f93b126e1a90b951bc1cf30468e0f811f5dc486
rosalyntan <rtan@nd.edu>
2016-05-04 03:01:06 -0400

    commented p2

--- p2.py
+++ p2.py
@@ -2,7 +2,7 @@
 
 #THIS IS PLAYER 2, THE MORE CLIENT-Y CLIENT, THE ONE WHO SHOOTS AT THINGS
 
-from modes import backgrounds, sesame, pirates, bball, otwist
+from modes import backgrounds, sesame, pirates, bball, otwist # four different modes to choose from--customizable gameplay
 
 import os
 import sys
@@ -21,9 +21,7 @@ import pygame
 from pygame.locals import *
 
 SERVER_HOST = 'localhost'
-#SERVER_HOST = 'student02.cse.nd.edu'
 SERVER_PORT = 40041
-
 
 class GameSpace:
 	def __init__(self):
@@ -43,28 +41,27 @@ class GameSpace:
 		self.quit = 0
 		self.ready = 0
 		self.acked = 0
+		# initial random background for waiting screen
 		tempMode = backgrounds[random.randint(0,3)]
 		self.bg = pygame.image.load("media/"+tempMode['background_image'])
 		self.bg = pygame.transform.scale(self.bg, tempMode['background_scale'])
 		self.gameOver = 0
-
-#		random.seed()
-
-		#3. start game loop
 	def setup(self):
 		self.tickNum = 0
 		self.score1 = 0
 		self.score2 = 0
+		# initialize game objects
 		self.rain = Rain(self)
 		self.player1 = Player1(self)
 		self.player2 = Player2(self)
 		self.p2body = Player2Prop(self)
 		self.endGame = GameOver(self)
-
+		# load background image
 		self.bg = pygame.image.load("media/"+self.mode['background_image'])
 		self.bg = pygame.transform.scale(self.bg, self.mode['background_scale'])
-
+	#3. start game loop
 	def game_loop(self):
+		# when the game ends
 		if self.gameOver == 1:
 			if self.score1 > 20:
 				self.endGame.display(1)
@@ -72,25 +69,12 @@ class GameSpace:
 				self.endGame.display(2)
 			pygame.display.flip()
 		elif self.ready ==1:
-			mx, my = pygame.mouse.get_pos()
-
-
-#			for bullet in self.player2.lasers:
-#				for guy in self.rain.drops:
-#					if collision(guy.rect.center, bullet.rect.center):
-#						self.rain.drops.remove(guy)
-#						self.player2.lasers.remove(bullet)
-#						self.score2+=1
-#						break
-
 			#4. clock tick regulation (framerate)
 			self.clock.tick(60)
 			
 			#5. handle user inputs
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
-			#		pygame.quit()
-			#		sys.exit()
 					self.quit()
 				if event.type == pygame.MOUSEBUTTONDOWN:
 					self.player2.tofire = 1
@@ -98,31 +82,33 @@ class GameSpace:
 					self.player2.tofire = 0
 
 		#6. send a tick to every game object
-	#	if self.ready == 1:
 			self.rain.tick()
 			self.player1.tick()
 			self.player2.tick()
 			self.tickNum+=1
 			for laser in self.player2.lasers:
 				laser.tick()
+			# send information to player 1 every three ticks
 			if self.acked == 1 and self.tickNum%3 == 0:
 				laserListx = []
 				laserListy = []
 				laserListxm = []
 				laserListym = []
+				# list of all laser x, y coordinates, as well as slopes
 				for laser in self.player2.lasers:
 					laserListx.append(laser.rect.centerx)
 					laserListy.append(laser.rect.centery)
 					laserListxm.append(laser.xm)
 					laserListym.append(laser.ym)
-				# player 2 should send over rotation information, score, laser locations
+				# player 2 sends over rotation information, score, laser locations, laser slopes
 				self.write(zlib.compress(pickle.dumps([self.player2.mx, self.player2.my, pickle.dumps(laserListx), pickle.dumps(laserListy), pickle.dumps(laserListxm), pickle.dumps(laserListym)])))
-				self.player2.fired = 0 # might not need this
+#				self.player2.fired = 0 # might not need this
 			self.acked = 1
 			#7. finally, display game object
 			self.screen.blit(self.bg, (0,0))
 			self.screen.blit(self.p2body.image, self.p2body.rect)
 			self.screen.blit(self.player1.image, self.player1.rect)
+			# blit lasers
 			for laser in self.player2.lasers:
 				self.screen.blit(laser.image, laser.rect)
 			self.screen.blit(self.player2.image, self.player2.rect)
@@ -140,17 +126,19 @@ class GameSpace:
 				TextRect2.center = [TextRect2.size[1]/2,150]
 			self.screen.blit(text2Surf, TextRect2)
 			self.screen.blit(textSurf, TextRect)
-			self.screen.blit(self.player1.box.image, self.player1.box.rect)	
+			self.screen.blit(self.player1.box.image, self.player1.box.rect)
+			# blit falling sky stuff
 			for guy in self.rain.drops:
 				self.screen.blit(guy.image, guy.rect)
 			pygame.display.flip()
+			# end-game condition
 			if self.score2 > 20 or self.score1 > 20:
 				self.gameOver = 1
 		else:
 			randback = random.randint(1,4)	
 			self.screen.blit(self.bg, (0, 0))
 			lt = pygame.font.Font('freesansbold.ttf', 50)
-			textSurf = lt.render("Waiting for Player 1", True, (5, 100, 5))
+			textSurf = lt.render("Waiting for Player 1", True, (255, 255, 255))
 			TextRect = textSurf.get_rect()
 			self.screen.blit(textSurf, TextRect)
 			pygame.display.flip()
@@ -162,55 +150,36 @@ class GameSpace:
 class GameOver(pygame.sprite.Sprite):
 	def __init__(self, gs = None):
 		self.gs = gs
-#		self.playAgain = pygame.image.load("media/penny.png") # need to change image
-#		self.quit = pygame.image.load("media/cookie.png") # need to change image
-#		self.playRect = self.playAgain.get_rect()
-#		self.quitRect = self.quit.get_rect()
-
-#		self.playRect.center = [175, 200]
-#		self.quitRect.center = [465, 200]
-#		self.messageSent = 0
 	def display(self, winner):
-#		if self.messageSent == 0:
-#			self.gs.write('game over')
-#			print 'game over sent'
-#			self.messageSent = 1
 		self.gs.screen.fill((0, 0, 0))
+		# player 1 wins
 		if winner == 1:
 			lt = pygame.font.Font('freesansbold.ttf', 30)
 			textSurf = lt.render("You lost :(", True, (255, 255, 255))
+		# player 2 wins
 		elif winner == 2:
 			lt = pygame.font.Font('freesansbold.ttf', 30)
 			textSurf = lt.render("You win! :)", True, (255, 255, 255))
 		textRect = textSurf.get_rect()
 		textRect.center = [200, 300]
 		self.gs.screen.blit(textSurf, textRect)
-#		self.gs.screen.blit(self.playAgain, self.playRect)
-#		self.gs.screen.blit(self.quit, self.quitRect)
+		# quit from game over screen
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.gs.quit()
-#				mx, my = pygame.mouse.get_pos()
-#				if mx > self.playRect.centerx-25 and mx < self.playRect.centerx+25 and my > self.playRect.centery-25 and my < self.playRect.centery+25:
-#					self.gs.gameOver = 0
-#					self.gs.ready = 0
-#					self.gs.acked = 0
-#					self.messageSent = 0
-#					self.gs.write('player 2 connected')
-#					print 'clicked play again'
-#				elif mx > self.quitRect.centerx-25 and mx < self.quitRect.centerx+25 and my > self.quitRect.centery-25 and my < self.quitRect.centery+25:
-#					print 'clicked quit'
-#					self.gs.quit = 1
 
+# holds list of Raindrops objects, the stuff that falls from the sky
 class Rain(pygame.sprite.Sprite):
 	def __init__(self, gs=None):
 		self.gs = gs
 		self.addNew = False
 		self.drops = []
 	def tick(self):
+		# make the raindrops fall down
 		for guy in self.drops:
 			guy.rect = guy.rect.move([0,1])
 
+# each individual sky thing
 class Raindrops(pygame.sprite.Sprite):
 	def __init__(self, x, y, gs = None):
 		pygame.sprite.Sprite.__init__(self)
@@ -219,6 +188,7 @@ class Raindrops(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.center = [x,y]
 
+# static player 2 character on the right side of the screen (only the arm moves, in a different class)
 class Player2Prop(pygame.sprite.Sprite):
 	def __init__(self, gs = None):
 		self.gs = gs
@@ -226,6 +196,7 @@ class Player2Prop(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.center = self.gs.mode['sb_location']
 
+# player 1 object--location info comes from player 1
 class Player1(pygame.sprite.Sprite):
 	def __init__(self, gs = None):
 		pygame.sprite.Sprite.__init__(self)
@@ -237,19 +208,20 @@ class Player1(pygame.sprite.Sprite):
 		self.box = Box(self.rect.center, self.gs)
 	def tick(self):
 		pass
-		if self.Moving == "R":
-			self.rect = self.rect.move([5,0])
-			self.box.rect = self.box.rect.move([5,0])	
-		elif self.Moving == "L":
-			self.rect = self.rect.move([-5,0])
-			self.box.rect = self.box.rect.move([-5,0])
-		if self.rect.center[0]<self.gs.mode['max_player_left']:
-			self.rect.center = [self.gs.mode['max_player_left'], self.rect.center[1]]
-			self.box.rect.center = [self.rect.center[0]+self.gs.mode['box_offset'][0], self.rect.center[1]+self.gs.mode['box_offset'][1]]
-		elif self.rect.center[0]>self.gs.mode['max_player_right']:
-			self.rect.center = [self.gs.mode['max_player_right'], self.rect.center[1]]
-			self.box.rect.center = [self.rect.center[0]+self.gs.mode['box_offset'][0], self.rect.center[1]+self.gs.mode['box_offset'][1]]
+#		if self.Moving == "R":
+#			self.rect = self.rect.move([5,0])
+#			self.box.rect = self.box.rect.move([5,0])	
+#		elif self.Moving == "L":
+#			self.rect = self.rect.move([-5,0])
+#			self.box.rect = self.box.rect.move([-5,0])
+#		if self.rect.center[0]<self.gs.mode['max_player_left']:
+#			self.rect.center = [self.gs.mode['max_player_left'], self.rect.center[1]]
+#			self.box.rect.center = [self.rect.center[0]+self.gs.mode['box_offset'][0], self.rect.center[1]+self.gs.mode['box_offset'][1]]
+#		elif self.rect.center[0]>self.gs.mode['max_player_right']:
+#			self.rect.center = [self.gs.mode['max_player_right'], self.rect.center[1]]
+#			self.box.rect.center = [self.rect.center[0]+self.gs.mode['box_offset'][0], self.rect.center[1]+self.gs.mode['box_offset'][1]]
 
+# player 1 catches things in here
 class Box(pygame.sprite.Sprite):
 	def __init__(self, center, gs = None):
 		pygame.sprite.Sprite.__init__(self)
@@ -260,8 +232,8 @@ class Box(pygame.sprite.Sprite):
 		self.y = center[1]+self.gs.mode['box_offset'][1]
 		self.rect.center = [self.x,self.y]
 
+# player 2 (the person running this file)
 class Player2(pygame.sprite.Sprite):
-
 	def __init__(self, gs=None):
 		pygame.sprite.Sprite.__init__(self)
 		self.realx = 1
@@ -278,11 +250,10 @@ class Player2(pygame.sprite.Sprite):
 		#if I can fire laser beams, this flag will say whether I should be firing them right now
 		self.tofire = 0
 		self.fired = 0
-
 	def tick(self):
 		#get the mouse x and y position on the screen
 		self.mx, self.my = pygame.mouse.get_pos()
-
+		# remove lasers when they go off-screen
 		for guy in self.lasers:
 			if guy.rect.center[0] < -20 or guy.rect.center[0] > 660:
 				self.lasers.remove(guy)	
@@ -298,6 +269,7 @@ class Player2(pygame.sprite.Sprite):
 			ySlope = self.realy-self.rect.center[1]
 				
 			total = math.fabs(xSlope)+math.fabs(ySlope)
+			# only allow one laser on the screen at a time--could change to be more to make it harder for player 1
 			if len(self.lasers) < 1:
 				self.lasers.append(Laser(self.rect.center[0], self.rect.center[1], xSlope/total, ySlope/total, self.gs))
 			self.tofire = 0
@@ -305,10 +277,12 @@ class Player2(pygame.sprite.Sprite):
 		else:	
 			#code to calculate the angle between my current direction and the mouse position (see math.atan2)
 			self.angle = math.atan2(self.my-self.rect.center[1],self.mx-self.rect.center[0])*-180/math.pi+211.5-self.gs.mode['angle_offset']
+			# rotate shooting arm
 			self.image = pygame.transform.rotate(self.orig_image, self.angle)
 			self.rect = self.image.get_rect(center = self.rect.center)
 			self.tofire = 0
 
+# the things flying across the screen from player 2
 class Laser(pygame.sprite.Sprite):
 	def __init__(self, xc=320, yc=240, xm=1, ym=1, gs=None):
 		pygame.sprite.Sprite.__init__(self)
@@ -320,10 +294,9 @@ class Laser(pygame.sprite.Sprite):
 		self.image = pygame.image.load("media/"+self.gs.mode['bullet_image'])
 		self.rect = self.image.get_rect()
 		self.rect.center=[xc,yc]
-
 	def tick(self):
+		# move lasers across the screen at angle they were shot at
 		self.rect = self.rect.move([self.xm,self.ym])
-	
 		
 def dist(x1, y1, x2, y2):
 	return ((y2-y1)**2+(x2-x1)**2)**.5
@@ -338,36 +311,36 @@ def collision(ball_center, bullet_center):
 class ClientConnection(Protocol):
 	def __init__(self, client):
 		self.client = client
-		self.queue = DeferredQueue()
 	def dataReceived(self, data):
-		if self.client.quit == 1:
-			print 'quit called'
-			self.transport.loseConnection()
+#		if self.client.quit == 1:
+#			print 'quit called'
+#			self.transport.loseConnection()
+		# detect which mode is chosen
 		if data == 'sesame':
-			print 'sesame'
 			self.client.mode = sesame
 			self.client.setup()
 			self.client.ready = 1
 		elif data=='pirates':
-			print 'pirates'
 			self.client.mode=pirates
 			self.client.setup()
 			self.client.ready = 1
 		elif data == 'bball':
-			print 'bball'
 			self.client.mode = bball
 			self.client.setup()
 			self.client.ready = 1
 		elif data == 'otwist':
-			print 'otwist'
 			self.client.mode = otwist
 			self.client.setup()
 			self.client.ready = 1
+		# pickled data is sent from player 1
 		else:
 			data =  pickle.loads(zlib.decompress(data))
+			# player 1 location
 			self.client.player1.rect.center = data[0]
 			self.client.player1.box.rect.center = data[1]
+			# player 1 score
 			self.client.score1 = data[2]
+			# rain drop locations
 			self.client.rain.drops = []
 			rainx = pickle.loads(data[3])
 			rainy = pickle.loads(data[4])
@@ -375,15 +348,15 @@ class ClientConnection(Protocol):
 			for x in rainx:
 				self.client.rain.drops.append(Raindrops(x, rainy[i], self.client))
 				i+=1
+			# player 2 score
 			self.client.score2=data[5]
 	def connectionMade(self):
 		self.transport.write('player 2 connected')
 	def connectionLost(self, reason):
-		print 'lost connection to ' + SERVER_HOST + ' port ' + str(SERVER_PORT)
 		reactor.stop()
-	def write(self, data):
+	def write(self, data): # connects to game space function
 		self.transport.write(data)
-	def quit(self):
+	def quit(self): # connects to game space function
 		self.transport.loseConnection()
 
 class ClientConnFactory(ClientFactory):
@@ -395,7 +368,7 @@ class ClientConnFactory(ClientFactory):
 		self.client.quit = proto.quit # sets quit function in GameSpace to connection's quit function
 		return proto
 
-
+# starts game loop
 if __name__ == '__main__':
 	gs = GameSpace()
 	lc = LoopingCall(gs.game_loop)
